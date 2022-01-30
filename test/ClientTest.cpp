be7aea9328d1bbfc959e87fd523b8eb5efd46f32
numlock soft <numlocksoft@gmail.com>
2022-01-30 13:20:10 +0300

    Add new unit tests.
    Signed-off-by: numlock soft <numlocksoft@gmail.com>

--- test/ClientTest.cpp
+++ test/ClientTest.cpp
@@ -34,7 +34,38 @@ void get_state_root_hash_test() {
   TEST_ASSERT(client.GetStateRootHash().state_root_hash != "");
 }
 
-TEST_LIST = {{"peers", get_peers_test},
-             {"root hash 1", get_state_root_hash_block_height_test},
-             {"root hash 2", get_state_root_hash_test},
-             {NULL, NULL}};
+/**
+ * @brief Check the "info_get_status" rpc function. Check the result variables.
+ *
+ */
+void get_status_info_test () {
+  Casper::Client client(CASPER_TEST_ADDRESS);
+  Casper::GetStatusInfoResult result = client.GetStatusInfo();
+
+  TEST_ASSERT(result.api_version != "");
+  TEST_ASSERT(result.chainspec_name != "");
+  TEST_ASSERT(result.starting_state_root_hash != "");
+  TEST_ASSERT(result.peerList.size() != 0);
+  TEST_ASSERT(result.build_version != "");
+  TEST_ASSERT(result.uptime != "");
+}
+
+/**
+ * @brief Check the "info_get_status" rpc function. Check the result variables.
+ *
+ */
+void get_block_transfers_test () {
+  Casper::Client client(CASPER_TEST_ADDRESS);
+  Casper::GetBlockTransfersResult result = client.GetBlockTransfers();
+
+  TEST_ASSERT(result.api_version != "");
+}
+
+TEST_LIST = {
+    { "peers", get_peers_test },
+    { "root hash 1", get_state_root_hash_block_height_test },
+    { "root hash 2", get_state_root_hash_test },
+    { "get status info", get_status_info_test },
+    { "get block transfers", get_block_transfers_test },
+    { NULL, NULL }
+};
